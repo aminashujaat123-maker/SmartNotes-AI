a7196f9a967c41359e74916f9b909ff8f2a54081
Amina Shujaat <aminashujaat123@gmail.com>
2026-08-05 16:12:23 +0500

    Version 1 & Version 2 - Landing page and Authentication UI

---
+++ app/__init__.py
@@ -0,0 +1,36 @@
+from flask import Flask
+
+from config import Config
+from app.extensions import db, bcrypt, login_manager
+
+
+def create_app():
+
+    app = Flask(
+        __name__,
+        template_folder="../templates",
+        static_folder="../static",
+    )
+
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    from app.models.user import User
+
+
+    @login_manager.user_loader
+    def load_user(user_id):
+     return User.query.get(int(user_id))
+
+    with app.app_context():
+        db.create_all()
+
+    from app.routes.main import main
+    from app.routes.auth import auth
+
+    app.register_blueprint(main)
+    app.register_blueprint(auth)
+
+    return app
---
+++ static/js/main.js
@@ -0,0 +1,1 @@
+console.log("SmartNotes AI Loaded");
